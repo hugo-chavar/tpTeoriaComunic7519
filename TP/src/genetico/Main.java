2a819d6c594166f0934fb9c77b6030ed5ad017bd
mellerster <concdiego@gmail.com>
2014-07-02 14:45:40 -0300

    agrego alguna explicación de algo gen

--- TP/src/genetico/Main.java
+++ TP/src/genetico/Main.java
@@ -1,17 +1,8 @@
 package genetico;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 
-public class Main {
-	final static Charset ENCODING = StandardCharsets.UTF_8;
+public class Main {	
 
 	public static void main(String[] args) {
 		System.out.println("TP de Redes Complejas / Sistemas Multiagente");
@@ -27,32 +18,7 @@ public class Main {
 		double tasaDeSeleccion = 0.1; // tasa de reproduccion es el mismo nro
 		double tasaDeMutacion = 0.01;
 		double minimaVariacionEntreGeneraciones = 0.00000001;
-		String archivo = "parametros.txt";
-		Path path = Paths.get(archivo);
-		if (Files.exists(path)) {
-			try {
-				List<String> params = Files.readAllLines(path, ENCODING);
-				for (String linea : params) {
-					String[] lineaSeparada = linea.split("=");
-					String nombre = lineaSeparada[0].trim();
-					String valor = lineaSeparada[1].trim();
-					if (nombre.equals("CantidadIndividuosEnPoblacion")) {
-						cantidadIndividuosEnPoblacion = Integer.parseInt(valor
-								.toString());
-					} else if (nombre.equals("TasaDeSeleccion")) {
-						tasaDeSeleccion = Double.parseDouble(valor);
-					} else if (nombre.equals("TasaDeMutacion")) {
-						tasaDeMutacion = Double.parseDouble(valor);
-					} else if (nombre
-							.equals("MinimaVariacionEntreGeneraciones")) {
-						minimaVariacionEntreGeneraciones = Double
-								.parseDouble(valor);
-					}
-
-				}
-			} catch (IOException e) {
-			}
-		}
+		
 
 		// Creo la generacion 0 de la poblacion
 		Poblacion pobl = new Poblacion(cantidadIndividuosEnPoblacion,
